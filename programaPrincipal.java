6d663d57f388f9e7519767d61e6046d9f2dc3ffa
Lucas Scola Gaioti <lucas10_scola@hotmail.com>
2025-05-19 21:36:12 -0300

    Update programaPrincipal.java

--- programaPrincipal.java
+++ programaPrincipal.java
@@ -3,63 +3,6 @@ package controeEstoqueTeste;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
-/*
-    class Produto {
-
-        String nomeProduto;
-        double valorProduto;
-        int quantidadeProduto;
-        int idProduto;
-
-        Produto( int idProduto, String nomeProduto, double valorProduto, int quantidadeProduto){
-            this.nomeProduto = nomeProduto;
-            this.valorProduto = valorProduto;
-            this.quantidadeProduto = quantidadeProduto;
-            this.idProduto = idProduto;
-        }
-
-        public int getidProduto() {
-            return idProduto;
-        }
-
-        public String getNome(){
-            return nomeProduto;
-        }
-
-        public String setNome(String nomeProduto){
-            return this.nomeProduto = nomeProduto;
-        }
-
-        public double getPreco(){
-            return valorProduto;
-        }
-
-        public double setPreco(double valorProduto){
-            return this.valorProduto = valorProduto;
-        }
-
-        public int getQnt(){
-            return quantidadeProduto;
-        }
-
-        public int setQntSoma (int novaQntSoma){
-            return this.quantidadeProduto = quantidadeProduto + novaQntSoma;
-        }
-
-        public int setQntSub (int novaQntSub){
-            return this.quantidadeProduto = quantidadeProduto - novaQntSub;
-        }
-
-        @Override
-        public String toString(){
-            return "ID : " + idProduto + " | Nome: " + nomeProduto + " | R$" + valorProduto + " | Quantia no estoque: " + quantidadeProduto;
-        }
-
-    }
-
- */
-
-
 
 public class programaPrincipal {
 
